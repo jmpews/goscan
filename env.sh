8364439ddf7b28d68a433624dcad800366ac32f3
jmpews <jmpews@gmail.com>
2016-12-01 10:36:04 +0800

    update README

--- env.sh
+++ env.sh
@@ -3,5 +3,9 @@ sudo locale-gen zh_CN.UTF-8
 sudo locale-gen zh_CN.UTF-8 en_US.UTF-8
 wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
+
 echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+echo 'GOPATH=/tmp' >> ~/.profile
 . ~/.profile
+
+go get github.com/pborman/uuid
